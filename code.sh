58e9f2cb775dfa01ff22072ef339015b4677c057
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2023-08-23 14:17:48 -0400

    Update code.sh

--- code.sh
+++ code.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Specify the source directory and HDFS destination path
-source_directory="/path/to/source/directory"
+# Specify the HDFS destination path
 hdfs_destination_path="/path/to/hdfs/destination"
 
-# Check if the source directory exists
-if [ ! -d "$source_directory" ]; then
-    echo "Source directory does not exist: $source_directory"
-    exit 1
-fi
+# Get the current working directory
+source_directory=$(pwd)
+
+# Get the hostname
+hostname=$(hostname)
 
 # Check if HDFS destination path exists, create if not
 hadoop fs -test -e "$hdfs_destination_path"
@@ -22,15 +21,23 @@ if [ $? -ne 0 ]; then
 fi
 
 # Find and transfer .log files to HDFS
-find "$source_directory" -type f -name "*.log" | while read -r log_file; do
+find "$source_directory" -maxdepth 1 -type f -name "*.log" | while read -r log_file; do
     log_filename=$(basename "$log_file")
     datetime=$(date +"%Y%m%d_%H%M%S")
-    hdfs_target_path="$hdfs_destination_path/${log_filename%.*}_$datetime.log"
+    hdfs_target_path="$hdfs_destination_path/${log_filename%.*}_${hostname}_$datetime.log"
 
     # Upload the log file to HDFS
     hadoop fs -put "$log_file" "$hdfs_target_path"
     if [ $? -eq 0 ]; then
         echo "Uploaded $log_filename to HDFS as $hdfs_target_path."
+        
+        # Remove the log file from the source directory
+        rm "$log_file"
+        if [ $? -eq 0 ]; then
+            echo "Removed $log_filename from source directory."
+        else
+            echo "Failed to remove $log_filename from source directory."
+        fi
     else
         echo "Failed to upload $log_filename to HDFS."
     fi
